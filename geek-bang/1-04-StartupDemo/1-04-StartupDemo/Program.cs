d5e18238ec0e3c23cdf34d1578c2e4cb596d40f3
stanleycheung <cn.staney@gmail.com>
2020-03-09 12:39:27 +0800

    Finished 1-04

--- geek-bang/1-04-StartupDemo/1-04-StartupDemo/Program.cs
+++ geek-bang/1-04-StartupDemo/1-04-StartupDemo/Program.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -16,11 +18,67 @@ namespace _1_04_StartupDemo
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// 执行顺序：
+        /// ConfigureWebHostDefaults ->
+        /// ConfigureHostConfiguration ->
+        /// ConfigureAppConfiguration ->
+        /// ConfigureServices / ConfigureLogging / Startup / Startup.ConfigureServices ->
+        /// Startup.Configure
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                {
+                    Console.WriteLine("ConfigureAppConfiguration");
+                })
+                .ConfigureServices(service =>
+                {
+                    Console.WriteLine("ConfigureServices");
+                })
+                .ConfigureHostConfiguration(builder =>
+                {
+                    Console.WriteLine("ConfigureHostConfiguration");
+                })
+                .ConfigureLogging(builder => {
+                    Console.WriteLine("ConfigureLogging");
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    Console.WriteLine("ConfigureWebHostDefaults");
+
+                    //Startup不是必须的
                     webBuilder.UseStartup<Startup>();
+
+                    #region 可以使用以下代码来代替
+                    //webBuilder.ConfigureServices(services =>
+                    //{
+                    //    Console.WriteLine("webBuilder.ConfigureServices");
+                    //    services.AddControllers();
+                    //});
+
+                    //webBuilder.Configure(app =>
+                    //{
+                    //    Console.WriteLine("webBuilder.Configure");
+
+                    //    app.UseHttpsRedirection();
+
+                    //    app.UseRouting();
+
+                    //    app.UseAuthorization();
+
+                    //    app.UseStaticFiles();
+
+                    //    app.UseWebSockets();
+
+                    //    app.UseEndpoints(endpoints =>
+                    //    {
+                    //        endpoints.MapControllers();
+                    //    });
+                    //});
+                    #endregion
                 });
     }
 }
--- geek-bang/1-04-StartupDemo/1-04-StartupDemo/Startup.cs
+++ geek-bang/1-04-StartupDemo/1-04-StartupDemo/Startup.cs
@@ -17,6 +17,7 @@ namespace _1_04_StartupDemo
     {
         public Startup(IConfiguration configuration)
         {
+            Console.WriteLine("Startup");
             Configuration = configuration;
         }
 
@@ -25,12 +26,18 @@ namespace _1_04_StartupDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            Console.WriteLine("Startup.ConfigureServices");
             services.AddControllers();
+            services.AddMvc();
+            services.AddAuthentication();
+            services.AddAuthorization();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            Console.WriteLine("Startup.Configure");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -41,6 +48,10 @@ namespace _1_04_StartupDemo
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseStaticFiles();
+
+            //app.UseWebSockets();
 
             app.UseEndpoints(endpoints =>
             {
